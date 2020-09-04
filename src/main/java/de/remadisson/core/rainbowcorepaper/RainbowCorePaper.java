a60975d97bb0fa8b35e5a3923aebdef9b0456e18
remadisson <maximilianloers@icloud.com>
2020-09-04 15:28:25 +0200

    initial Commit

---
+++ src/main/java/de/remadisson/core/rainbowcorepaper/RainbowCorePaper.java
@@ -0,0 +1,17 @@
+package de.remadisson.core.rainbowcorepaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RainbowCorePaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
